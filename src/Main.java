637ca848aca6660501306f468e41863040e7b83b
Enes Kocer <enes0539@stud.kea.dk>
2022-09-09 11:29:01 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 12;
+        String name = "Mandu";
+        double weight = 9.45;
+        boolean isCat = true;
+
+        System.out.printf("%s er %d år gammel og vejer %2f kg.", name, age, weight, isCat);
+
+        System.out.println("\u0043\u006f\u006f\u006c\u263a");
+
+        System.out.println("Så er der kage |");
+
+        System.out.println("Den er \u001B[31mrød\u001B[0m.");
+
+    }
+}
